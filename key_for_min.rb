23eef3d6a9f7992ad81141b86d6fd7b38bdcb0c2
Benjamin Stein <whereyouben@gmail.com>
2020-10-20 21:30:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@ def key_for_min_value(name_hash)
   num = 0
   minKey = :placeholder
   name_hash.each do |key, value| 
-    num = value 
     if value < num 
       minKey = key 
     end 
+    num = value 
   end 
   minKey
 end
