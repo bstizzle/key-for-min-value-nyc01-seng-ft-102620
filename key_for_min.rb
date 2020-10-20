bc2187000c45a866874e6e40454424b26008871b
Benjamin Stein <whereyouben@gmail.com>
2020-10-20 21:41:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   prevNum = 0
   minKey = nil
   name_hash.each do |key, value| 
-    num = value  
+    minKey = key  
     if value <= prevNum 
       minKey = key 
     end 
